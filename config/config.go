885fa4d91ab2e77bae9d4da33cd462abf9519f15
Rancho <rancho.z@binance.com>
2021-12-01 18:11:00 +0800

    add logger

--- config/config.go
+++ config/config.go
@@ -1,16 +1,17 @@
 package config
 
 import (
-    "io/ioutil"
-    "log"
-    "path/filepath"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+    "runtime"
 
     "gopkg.in/yaml.v3"
 )
 
 const (
-	configFilePath = "config/config.yaml"
-    privateConfigFilePath = "config/config.private.yaml"
+	configFilePath        = "/config.yaml"
+	privateConfigFilePath = "/config.private.yaml"
 )
 
 type binanceConfig struct {
@@ -18,32 +19,43 @@ type binanceConfig struct {
 	Secret string `yaml:"secret"`
 }
 
+type logConfig struct {
+	LogSavePath string `yaml:"log_save_path"`
+	LogFileName string `yaml:"log_file_name"`
+	LogFileExt  string `yaml:"log_file_ext"`
+}
+
 type config struct {
 	Binance *binanceConfig `yaml:"binance"`
+	Log     *logConfig     `yaml:"log"`
 }
 
 var Config = &config{}
 
 func readYamlConfig(configPath string) {
-    yamlFile, err := filepath.Abs(configPath)
-    if err != nil {
-        log.Fatalf("invalid config file path, err: %v", err)
-    }
-    content, err := ioutil.ReadFile(yamlFile)
-    if err != nil {
-        log.Fatalf("read config file fail, err: %v", err)
-    }
-    err = yaml.Unmarshal(content, Config)
-    if err != nil {
-        log.Fatalf("config file unmarshal fail, err: %v", err)
-    }
+	yamlFile, err := filepath.Abs(configPath)
+	if err != nil {
+		log.Fatalf("invalid config file path, err: %v", err)
+	}
+	content, err := ioutil.ReadFile(yamlFile)
+	if err != nil {
+		log.Fatalf("read config file fail, err: %v", err)
+	}
+	err = yaml.Unmarshal(content, Config)
+	if err != nil {
+		log.Fatalf("config file unmarshal fail, err: %v", err)
+	}
 }
 
 func init() {
-    readYamlConfig(configFilePath)
+    _, file, _, _ := runtime.Caller(0)
+    rootPath := filepath.Dir(file)
 
-    if Config.Binance.Key == "" || Config.Binance.Secret == "" {
-        // read private config
-        readYamlConfig(privateConfigFilePath)
-    }
+	readYamlConfig(rootPath + configFilePath)
+
+    // read private sensitive configs
+	if Config.Binance.Key == "" || Config.Binance.Secret == "" {
+		// read private config
+		readYamlConfig(rootPath + privateConfigFilePath)
+	}
 }
--- internal/port.adapter/dependency/http/binance.go
+++ internal/port.adapter/dependency/http/binance.go
@@ -1,10 +1,12 @@
 package http
 
 import (
+    "context"
     "fmt"
     "io/ioutil"
-    "log"
     "net/http"
+
+    "go-hexagonal/util/logger"
 )
 
 /**
@@ -15,12 +17,13 @@ import (
 func Ping() string {
     resp, err := http.Get(fmt.Sprintf("%s/ping", BinanceAPIV3))
     if err != nil {
-        log.Fatalln(err)
+        logger.Log.Errorf(context.Background(), "ping binanceAPIV3 err: %v", err)
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        log.Fatalln(err)
+        logger.Log.Errorf(context.Background(), "read ping binanceAPIV3 body err: %v", err)
     }
+
     return string(body)
 }
